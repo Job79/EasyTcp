099d9a97cb1bc180a6e901ec8b14d1c2754a7719
job79 <jobse@pm.me>
2020-04-29 14:46:06 +0200

    Added StreamAsyncUtil

--- EasyTcp3/EasyTcp3/ClientUtils/Async/StreamAsyncUtil.cs
+++ EasyTcp3/EasyTcp3/ClientUtils/Async/StreamAsyncUtil.cs
@@ -3,9 +3,9 @@ using System.IO;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 
-namespace EasyTcp3.ClientUtils
+namespace EasyTcp3.ClientUtils.Async
 {
-    public static class StreamUtil
+    public static class StreamAsyncUtil
     {
         /// <summary>
         /// Send a stream to the remote host.
@@ -15,20 +15,20 @@ namespace EasyTcp3.ClientUtils
         /// <param name="stream">Stream to send to the host</param>
         /// <param name="bufferSize"></param>
         /// <exception cref="InvalidDataException">Stream is not readable</exception>
-        public static void SendStream(this EasyTcpClient client, Stream stream, int bufferSize = 1024)
+        public static async Task SendStreamAsync(this EasyTcpClient client, Stream stream, int bufferSize = 1024)
         {
             if (!stream.CanRead) throw new InvalidDataException("Stream is not readable");
             
-            using var networkStream = new NetworkStream(client.BaseSocket);
-            networkStream.Write(BitConverter.GetBytes(stream.Length));
+            await using var networkStream = new NetworkStream(client.BaseSocket);
+            await networkStream.WriteAsync(BitConverter.GetBytes(stream.Length));
 
             var buffer = new byte[bufferSize];
             int read;
 
-            while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
-                networkStream.Write(buffer, 0, read);
+            while ((read = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                await networkStream.WriteAsync(buffer, 0, read);
         }
-
+        
         /// <summary>
         /// Receive a stream from a remote host,
         /// This can only be done in an OnReceive event.
@@ -37,23 +37,23 @@ namespace EasyTcp3.ClientUtils
         /// <param name="stream">Stream to write receiving stream to</param>
         /// <param name="bufferSize"></param>
         /// <exception cref="InvalidDataException">Stream is not writable</exception>
-        public static void ReceiveStream(this Message message, Stream stream, int bufferSize = 1024)
+        public static async Task ReceiveStream(this Message message, Stream stream, int bufferSize = 1024)
         {
             if (!stream.CanWrite) throw new InvalidDataException("Stream is not writable");
             
-            using var networkStream = new NetworkStream(message.Client.BaseSocket);
+            await using var networkStream = new NetworkStream(message.Client.BaseSocket);
             
             var length = new byte[8];
-            networkStream.Read(length, 0, length.Length);
+            await networkStream.ReadAsync(length, 0, length.Length);
             var totalBytes = BitConverter.ToInt64(length);
             
             var buffer = new byte[bufferSize];
             int read, totalReceivedBytes = 0;
 
             while (totalReceivedBytes < totalBytes &&
-                   (read = networkStream.Read(buffer, 0, buffer.Length)) > 0)
+                   (read = await networkStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
             {
-                stream.Write(buffer, 0, read);
+                await stream.WriteAsync(buffer, 0, read);
                 totalReceivedBytes += read;
             }
         }
