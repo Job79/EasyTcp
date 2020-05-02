42efc4e3497679d4f88a5b458fbf891a76ff4d47
job79 <jobse@pm.me>
2020-05-02 17:21:03 +0200

    Removed multiple attributes because it was not working

--- EasyTcp3/EasyTcp3.Actions/EasyTcpAction.cs
+++ EasyTcp3/EasyTcp3.Actions/EasyTcpAction.cs
@@ -5,7 +5,7 @@ namespace EasyTcp3.Actions
     /// <summary>
     /// Attribute type for EasyTcpActions, only methods with this attribute will get loaded as actions
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Method)]
     public class EasyTcpAction : Attribute
     {
         /// <summary>
@@ -15,24 +15,6 @@ namespace EasyTcp3.Actions
         public int ActionCode { get; set; }
 
         public EasyTcpAction(int actionCode)  => ActionCode = actionCode;
-
-        
-        //TODO
-        /*
-        public EasyTcpAction(string actionCode) => ActionCode = Hash(actionCode);
-
-        /// <summary>
-        /// jdba2 hashing function
-        /// http://www.cse.yorku.ca/~oz/hash.html
-        /// https://softwareengineering.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed
-        /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        private int Hash(string str)
-        {
-            int hash = 5381;
-            foreach (var t in str) hash = ((hash << 5) + hash) ^ (byte)t;
-            return hash;
-        }*/
+        public EasyTcpAction(string actionCode) => ActionCode = ActionsCore.ToActionCode(actionCode);
     }
 }
