170d65b9c89379f017f56fd79802994b84faa970
job79 <jobse@pm.me>
2020-05-05 13:17:01 +0200

    Added .close to compression

--- EasyTcp3/EasyTcp3/Shared/Compression.cs
+++ EasyTcp3/EasyTcp3/Shared/Compression.cs
@@ -3,18 +3,19 @@ using System.IO.Compression;
 
 namespace EasyTcp3.Shared
 {
-    internal class Compression
+    internal static class Compression
     {
         /// <summary>
         /// Compress byte[] of data using GZIP
         /// </summary>
         /// <param name="data">uncompressed data</param>
         /// <returns>compressed data</returns>
-        static byte[] Compress(byte[] data)
+        internal static byte[] Compress(byte[] data)
         {
             using var compressedStream = new MemoryStream();
             using var zipStream = new GZipStream(compressedStream, CompressionMode.Compress);
             zipStream.Write(data, 0, data.Length);
+            zipStream.Close();
             return compressedStream.ToArray();
         }
 
@@ -23,7 +24,7 @@ namespace EasyTcp3.Shared
         /// </summary>
         /// <param name="data">compressed data</param>
         /// <returns>decompressed data</returns>
-        static byte[] Decompress(byte[] data)
+        internal static byte[] Decompress(byte[] data)
         {
             using var compressedStream = new MemoryStream(data);
             using var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress);
