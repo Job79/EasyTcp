9253ea4033e946a49d0ac0651614e0b17319f607
job79 <jobse@pm.me>
2020-05-05 12:01:56 +0200

    Upload compression class

---
+++ EasyTcp3/EasyTcp3/Shared/Compression.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using System.IO.Compression;
+
+namespace EasyTcp3.Shared
+{
+    internal class Compression
+    {
+        /// <summary>
+        /// Compress byte[] of data using GZIP
+        /// </summary>
+        /// <param name="data">uncompressed data</param>
+        /// <returns>compressed data</returns>
+        static byte[] Compress(byte[] data)
+        {
+            using var compressedStream = new MemoryStream();
+            using var zipStream = new GZipStream(compressedStream, CompressionMode.Compress);
+            zipStream.Write(data, 0, data.Length);
+            return compressedStream.ToArray();
+        }
+
+        /// <summary>
+        /// Decompress byte[] of data using GZIP
+        /// </summary>
+        /// <param name="data">compressed data</param>
+        /// <returns>decompressed data</returns>
+        static byte[] Decompress(byte[] data)
+        {
+            using var compressedStream = new MemoryStream(data);
+            using var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress);
+            using var resultStream = new MemoryStream();
+            zipStream.CopyTo(resultStream);
+            return resultStream.ToArray();
+        }
+    }
+}
