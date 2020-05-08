6564d25a92b63cacc52b9e9488f8792cbf89eaa2
Job <jobse@pm.me>
2020-05-08 17:00:40 +0200

    Added tests for passwords

--- EasyTcp3/EasyTcp3.Test/Encryption/EncryptAndDecrypt.cs
+++ EasyTcp3/EasyTcp3.Test/Encryption/EncryptAndDecrypt.cs
@@ -10,11 +10,33 @@ namespace EasyTcp3.Test.Encryption
         public void TestEncryptionAndDecryption()
         {
             var encryption = new EasyEncrypt();
+
             var testData = "testData";
             var encrypted = encryption.Encrypt(testData);
-            Console.WriteLine(encrypted);
             var decrypted = encryption.Decrypt(encrypted);
-            Assert.AreEqual(testData,decrypted);
+            Assert.AreEqual(testData, decrypted);
+        }
+
+        [Test]
+        public void TestGenerateKey()
+        {
+            var key = EasyEncrypt.CreateKey("password", "test12345678", 256);
+            var key2 = EasyEncrypt.CreateKey("password2", "test12345678", 256);
+            Console.WriteLine(Convert.ToBase64String(key));
+            Assert.AreEqual("criGtQT7ZrS7SoRMvaXK4yuRm0XtTUJA7937nIzXa9Q=", Convert.ToBase64String(key));
+            Assert.AreNotEqual("criGtQT7ZrS7SoRMvaXK4yuRm0XtTUJA7937nIzXa9Q=", Convert.ToBase64String(key2));
+        }
+
+        [Test]
+        public void TestGenerateKeyConstructor()
+        {
+            var encryption = new EasyEncrypt("password", "salt12345678");
+
+            var testData = "testData";
+            var encrypted = encryption.Encrypt(testData);
+            var decrypted = encryption.Decrypt(encrypted);
+            Assert.AreEqual(32, encryption.GetKey().Length);
+            Assert.AreEqual(testData, decrypted);
         }
     }
 }
