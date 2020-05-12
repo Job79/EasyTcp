24ed36f13afd8d4958f5a0eb87c5e865a2b318b5
job79 <jobse@pm.me>
2020-05-12 20:02:43 +0200

    Added documentation to IEasyTcpPacket

--- EasyTcp3/EasyTcp3/IEasyTcpPacket.cs
+++ EasyTcp3/EasyTcp3/IEasyTcpPacket.cs
@@ -1,7 +1,15 @@
 namespace EasyTcp3
 {
+    /// <summary>
+    /// Interface used by multiple EasyTcpFunctions,
+    /// implement when a class needs to be send over TCP
+    /// </summary>
     public interface IEasyTcpPacket
     {
+        /// <summary>
+        /// get => return class as byte[]
+        /// set => create class from byte[]
+        /// </summary>
         public byte[] Data { get; set; }
     }
 }
