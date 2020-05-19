4949b09c7e722955e0c0c10574ff021f222a9d07
job79 <jobse@pm.me>
2020-05-19 20:14:14 +0200

    Removed unused import

--- EasyTcp3/EasyTcp3/Server/EasyTcpServer.cs
+++ EasyTcp3/EasyTcp3/Server/EasyTcpServer.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
@@ -120,7 +119,6 @@ namespace EasyTcp3.Server
             }
 
             ConnectedClients.Clear();
-
             BaseSocket.Dispose();
             BaseSocket = null;
         }
