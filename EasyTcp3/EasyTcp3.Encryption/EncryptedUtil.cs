503790df64b62582ce6bd7ba3cea2db8c269c647
Job Jobse <jobse@pm.me>
2020-06-22 19:59:15 +0200

    Added encryptionUtil.cs

--- EasyTcp3/EasyTcp3.Encryption/EncryptedUtil.cs
+++ EasyTcp3/EasyTcp3.Encryption/EncryptedUtil.cs
@@ -1,5 +1,10 @@
+using System.Security.Cryptography.X509Certificates;
 using EasyEncrypt2;
+using EasyTcp.Encryption.Protocols.Tcp;
+using EasyTcp.Encryption.Protocols.Tcp.Ssl;
+using EasyTcp3;
 using EasyTcp3.EasyTcpPacketUtils;
+using EasyTcp3.Server;
 
 namespace EasyTcp.Encryption
 {
@@ -30,6 +35,28 @@ namespace EasyTcp.Encryption
         {
             data.Data = encryption.Decrypt(data.Data);
             return data;
+        }
+
+        /// <summary>
+        /// Shortcut for enabling encryption 
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="encrypt"></param>
+        public static EasyTcpClient UseEncryption(this EasyTcpClient client, EasyEncrypt encrypt)
+        {
+            client.Protocol = new EncryptedPrefixLengthProtocol(encrypt);
+            return client;
+        }
+
+        /// <summary>
+        /// Shortcut for enabling encryption 
+        /// </summary>
+        /// <param name="server"></param>
+        /// <param name="encrypt"></param>
+        public static EasyTcpServer UseEncryption(this EasyTcpServer server, EasyEncrypt encrypt)
+        {
+            server.Protocol = new EncryptedPrefixLengthProtocol(encrypt);
+            return server;
         }
     }
 }
