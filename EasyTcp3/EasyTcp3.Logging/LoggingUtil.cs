06aaeee45a3b85a932f236b41669f0aa2699c33f
Job Jobse <jobse@pm.me>
2020-06-29 11:52:17 +0200

    Upload EasyTcp3.Logging and logging examples

--- EasyTcp3/EasyTcp3.Logging/LoggingUtil.cs
+++ EasyTcp3/EasyTcp3.Logging/LoggingUtil.cs
@@ -3,33 +3,63 @@ using EasyTcp3.Server;
 
 namespace EasyTcp3.Logging
 {
-    //TODO add documentation
+    /// <summary>
+    /// Class with functions to enable logging for 1 server/client 
+    /// </summary>
     public static class LoggingUtil
     {
-        public static T UseClientLogging<T>(this T client, Action<LoggingMessage> logger) where T : EasyTcpClient
+        /// <summary>
+        /// Enable logging for client
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="logger"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T UseCustomClientLogging<T>(this T client, Action<LogMessage> logger) where T : EasyTcpClient
         {
-            client.OnDataReceive += (sender, message) => logger(new LoggingMessage(message, true));
-            client.OnDataSend += (sender, message) => logger(new LoggingMessage(message, false));
+            client.OnDataReceive += (sender, message) => logger(new LogMessage(message, LoggingType.DataReceived, sender));
+            client.OnDataSend += (sender, message) => logger(new LogMessage(message, LoggingType.DataSend, sender));
+            client.OnDisconnect += (sender, c) => logger(new LogMessage(c, LoggingType.ClientDisconnected, sender));
+            client.OnConnect += (sender, c) => logger(new LogMessage(c, LoggingType.ClientConnected, sender));
+            client.OnError += (sender, exception) => logger(new LogMessage(exception, sender));
             return client;
         }
         
-        public static T UseServerLogging<T>(this T server, Action<LoggingMessage> logger) where T : EasyTcpServer 
+        /// <summary>
+        /// Enable logging for server
+        /// </summary>
+        /// <param name="server"></param>
+        /// <param name="logger"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T UseCustomServerLogging<T>(this T server, Action<LogMessage> logger) where T : EasyTcpServer 
         {
-            server.OnDataReceive += (sender, message) => logger(new LoggingMessage(message, true));
-            server.OnDataSend += (sender, message) => logger(new LoggingMessage(message, false));
+            server.OnDataReceive += (sender, message) => logger(new LogMessage(message, LoggingType.DataReceived, sender));
+            server.OnDataSend += (sender, message) => logger(new LogMessage(message, LoggingType.DataSend, sender));
+            server.OnConnect += (sender, client) => logger(new LogMessage(client, LoggingType.ClientConnected, sender));
+            server.OnDisconnect += (sender, client) => logger(new LogMessage(client, LoggingType.ClientDisconnected, sender));
+            server.OnError += (sender, exception) => logger(new LogMessage(exception, sender));
             return server;
         }
 
+        /// <summary>
+        /// Enable custom logging for client
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="logger"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static T UseClientLogging<T>(this T client, Action<string> logger) where T : EasyTcpClient
-            => client.UseClientLogging(message => logger(LoggingOutputGenerator.GenerateOutput(message)));
+            => client.UseCustomClientLogging(message => logger(message.ToString()));
 
+        /// <summary>
+        /// Enable custom logging for server
+        /// </summary>
+        /// <param name="server"></param>
+        /// <param name="logger"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static T UseServerLogging<T>(this T server, Action<string> logger) where T : EasyTcpServer 
-            => server.UseServerLogging(message => logger(LoggingOutputGenerator.GenerateOutput(message)));
-        
-        public static T UseClientConsoleLogging<T>(this T client) where T : EasyTcpClient
-            => client.UseClientLogging((string message) => Console.WriteLine(message));
-        
-        public static T UseServerConsoleLogging<T>(this T server) where T : EasyTcpServer 
-            => server.UseServerLogging((string message) => Console.WriteLine(message));
+            => server.UseCustomServerLogging(message => logger(message.ToString()));
     }
 }
